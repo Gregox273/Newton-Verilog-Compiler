6fedcdadfc0f64750ed6db13fdb4c54de8179da6
Greg Brooks <gregox273@gmail.com>
2019-02-06 09:30:56 +0000

    Modify makefile to synthesise all .v files, begin debugging clz.v

--- c_compiler/verilog/clz.v
+++ c_compiler/verilog/clz.v
@@ -1,8 +1,11 @@
-// Count leading zeros
-// Source:
-//    V. Oklobdzija, “An Implementation Algorithm and Design of a Novel
-//    Leading Zero Detector Circuit”, 26th IEEE Asilomar Conference on Signals,
-//    Systems, and Computers, 1992, pp. 391- 395.
+/* Count leading zeros
+ * Source:
+ *    V. Oklobdzija, “An Implementation Algorithm and Design of a Novel
+ *    Leading Zero Detector Circuit”, 26th IEEE Asilomar Conference on Signals,
+ *    Systems, and Computers, 1992, pp. 391- 395.
+ *
+ * Block diagram is in project notebook (13th December)
+ */
 `include "utils.vh"
 //`include "rng.vh"
 
@@ -46,7 +49,7 @@ endmodule  // clz_encode
  * vg -- output valid bit
  * pg -- output position bits
  *
- * N  -- dimension of this hierarchical layer (output has width log2(N))
+ * bits_out  -- number ofposition bits at output
  */
 module clz_merge_N(
   input vl,
@@ -56,8 +59,7 @@ module clz_merge_N(
   output vg,
   output [0:bits_out-1] pg);
 
-  parameter N = 4;
-  localparam bits_out = `CLOG2(N);
+  parameter bits_out = 2;
 
   always@* begin
     vg = vl | vr;
@@ -89,19 +91,34 @@ module clz(
   localparam half_bits_in = 0.5*bits_in;
   localparam bits_out = `CLOG2(bits_in);
 
-  // Generate the necessary wires
-  genvar i_wN;
+  /* Generate the necessary wires
+   *
+   * i_wN -- hierarchical layer of wires being generated
+   *         = 1 at the outputs to the two bit LZD units
+   *         = bits_out-1 at the module output
+   * i_pN -- index of position bit wire
+   */
+  genvar i_wN, i_pN;
   generate
     for(i_wN = 0; i_wN < bits_out; i_wN = i_wN + 1) begin : lzd_wN
-        wire [0:half_bits_in/(2**i_wN)] v;
-        wire [0:(i_wN + 1)*half_bits_in/(2**i_wN)] p;
+      wire v[0:half_bits_in/(2**i_wN)-1];
+      wire p[0:(i_wN + 1)*half_bits_in/(2**i_wN)-1];
+
+      // If at the output level of the hierarchy, map wires to output
+      if (i_wN == bits_out-1) begin
+        assign valid = lzd_wN[i_wN].v[0];
+        for(i_pN = 0; i_pN < bits_out; i_pN = i_pN + 1) begin : lzd_wN_pN
+          // TODO: check endianness is right way round
+          assign out[bits_out-1-i_pN] = lzd_wN[i_wN].p[i_pN];
+        end
+      end
     end
   endgenerate
 
-  assign valid = lzd_wN[bits_out-1].v[0];
-  assign out = lzd_wN[bits_out-1].p;
-
-  // Generate 2 bit leading zero detectors
+  /* Generate 2 bit leading zero detectors
+   *
+   * i_lzd2 -- index of 2 bit LZD
+   */
   genvar i_lzd2;
   generate
     for(i_lzd2 = 0; i_lzd2 < half_bits_in; i_lzd2 = i_lzd2 + 1) begin : lzd2
@@ -113,19 +130,35 @@ module clz(
     end
   endgenerate
 
-  // Generate N bit lzd blocks
+  /* Generate N bit lzd blocks
+  *
+  * i_N  -- hierarchical layer being generated
+  * i_Nx -- index of module within hierarchical layer i_N
+  */
   genvar i_N, i_Nx;
   generate
     for(i_N = 1; i_N < bits_out; i_N = i_N + 1) begin : lzd_N
       for(i_Nx = 0; i_Nx < half_bits_in/(2**i_N); i_Nx = i_Nx + 1) begin : lzd_Nx
-        clz_merge_N #(.N({i_N})) clz_Nx (  // 2**(I_N+1)
+        if(i_N == 1) begin
+          clz_merge_N #(.bits_out(i_N+1)) clz_Nx (  // 2**(I_N+1)
             .vl(lzd_wN[i_N-1].v[2*i_Nx]),
             .vr(lzd_wN[i_N-1].v[2*i_Nx+1]),
-            .pl(lzd_wN[i_N-1].v[(2*i_N*i_Nx) : (2*i_N*i_Nx + i_N - 1)]),
-            .pr(lzd_wN[i_N-1].v[2*i_N*i_Nx + i_N : 2*i_N*i_Nx + 2*i_N - 1]),
+            .pl(lzd_wN[i_N-1].p[2*i_N*i_Nx]),
+            .pr(lzd_wN[i_N-1].p[2*i_N*i_Nx + i_N]),
             .vg(lzd_wN[i_N].v[i_Nx]),
             .pg(lzd_wN[i_N].p[(i_N+1)*i_Nx : (i_N+1)*(i_Nx+1)-1])
           );
+        end
+        else begin
+          clz_merge_N #(.N(i_N+1)) clz_Nx (  // 2**(I_N+1)
+            .vl(lzd_wN[i_N-1].v[2*i_Nx]),
+            .vr(lzd_wN[i_N-1].v[2*i_Nx+1]),
+            .pl(lzd_wN[i_N-1].p[(2*i_N*i_Nx) : (2*i_N*i_Nx + i_N - 1)]),
+            .pr(lzd_wN[i_N-1].p[2*i_N*i_Nx + i_N : 2*i_N*i_Nx + 2*i_N - 1]),
+            .vg(lzd_wN[i_N].v[i_Nx]),
+            .pg(lzd_wN[i_N].p[(i_N+1)*i_Nx : (i_N+1)*(i_Nx+1)-1])
+          );
+        end
       end
     end
   endgenerate
