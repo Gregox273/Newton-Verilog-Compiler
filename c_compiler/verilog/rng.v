a54f559608ce33f04f58529716aecd490cd439d1
Greg Brooks <gregox273@gmail.com>
2019-05-23 23:48:14 +0100

    Work on RNG to the point where it synthesises

--- c_compiler/verilog/rng.v
+++ c_compiler/verilog/rng.v
@@ -4,59 +4,11 @@
 `include "utils.vh"
 `include "clz.v"
 
-/*
- * Generate uniform random numbers until nonzero exponent section is reached
- * TODO: finish implementing
- */
-module rng_nonzero_exp_urng (
-	input wire clk, rst,
-	input wire comparator_output,
-	output reg [BX - 1:0] out,
-	output reg valid
-	);
-
-	parameter BX = `URNG_BX;
-
-	reg urng_rst;
-	wire [BX - 1:0] urng_out;
-	wire [BX - 1:0] urng_valid_vector;
-
-	uniform_rng #(.N(BX)) urng(
-		.comparator_output(comparator_output),
-		.clk(clk),
-		.rst(urng_rst),
-		.out(urng_out),
-		.valid(urng_valid_vector)
-		);
-
-	always @ ( posedge clk ) begin
-		if (rst) begin
-			out <= 0;
-			exp_valid <= 0;
-			urng_rst <= 1;
-		end
-		else begin
-			valid <= urng_valid_vector[BX-1];
-			if (urng_valid_vector[BX-1]) begin
-				out <= urng_out;
-				if (urng_out[BX - 3 : MANT_BW] == 0) begin
-		    	// Consume another random number if exponent part is zero
-		      urng_rst <= 1;
-		    end
-			end
-			else if (urng_rst) begin
-				urng_rst <= 0;
-			end
-		end
-	end
-endmodule // rng_nonzero_exp_urng
-
 module rng_uniform_to_float(
-	input clk,
-	input rst,
+	input clk, rst, urng_valid,
 	input [BX - 1:0] uniform,
 	output reg [BX - 1:0] floating,
-	output reg data_valid,
+	output reg float_valid,
 	output reg rst_urng
 	);
 
@@ -64,9 +16,11 @@ module rng_uniform_to_float(
 	parameter BY = `RNG_BY;
 	parameter K  = `RNG_K;
 	parameter MANT_BW  = `RNG_MANT_BW;
-	parameter EXP_BW   = `RNG_EXP_BW;
 	parameter G_OCT = `RNG_GROWING_OCT;
 	parameter D_OCT = `RNG_DIMINISHING_OCT;
+
+	// If 'RNG_EXP_BW is odd, make even to fit into clz module
+	parameter EXP_BW = (`RNG_EXP_BW % 2) ? `RNG_EXP_BW + 1 : `RNG_EXP_BW;
 
 	wire [`CLOG2(EXP_BW)-1:0] clz_out;
 	wire clz_valid;
@@ -74,48 +28,64 @@ module rng_uniform_to_float(
 	reg [`CLOG2(`MAX(G_OCT,D_OCT))-1:0] max_exp;
 	reg [BX - 1:0] uniform_pipe;
 
-	//assign max_exp = ( == 1) ? D_OCT : G_OCT ;  // Depends on part bit
-	always@(uniform_pipe[BX - 2]) begin
-		if(uniform_pipe[BX - 2] == 1) begin
-			max_exp = D_OCT;
-		end else begin
-			max_exp = G_OCT;
-		end
-	end
-
-	clz_clk #(.bits_in(EXP_BW)) count_leading_zeros (
-		.b(uniform_buf[BX - 3 : MANT_BW]),
-		.clk(clk),
-		.rst(rst),
-		.vout(clz_valid),
-		.pout(clz_out)
-	);
-
-	always@(posedge clk) begin
+	always @ ( posedge clk ) begin
 		if (rst) begin
 			floating <= 0;
-			data_valid <= 0;
-			ready <= 0;
+			float_valid <= 0;
+			rst_urng <= 0;
 			max_exp <= 0;
 			uniform_pipe <= 0;
 		end
-		else begin
+
+		else if(!float_valid) begin
+		// Keep requesting new random numbers until exponent part is nonzero
+			if (urng_valid) begin
+				// A valid uniform random bit vector has been received
+				if (uniform[BX - 3 : MANT_BW] == 0) begin
+		    	// Consume another random number if exponent part is zero
+		      rst_urng <= 1;
+		    end
+				// else can stop consuming random numbers
+			end
+
+			else if (rst_urng) begin
+				rst_urng <= 0;  // Clear reset signal
+			end
+
+		// Process an incoming uniform random number
 			uniform_pipe <= uniform;  // delay by one clock cycle to synchronise with clz module
+			//assign max_exp = ( == 1) ? D_OCT : G_OCT ;  // Depends on part bit
+			if(uniform_pipe[BX - 2] == 1) begin
+				max_exp <= D_OCT;
+			end else begin
+				max_exp <= G_OCT;
+			end
 			if (floating[BX - 3 : MANT_BW] + clz_out > max_exp) begin
 				floating[BX - 3 : MANT_BW] <= max_exp;  // Exponent
+				float_valid <= 1;
 			end else begin
 				floating[BX - 3 : MANT_BW] <= floating[BX - 3 : MANT_BW] + clz_out;
+				float_valid <= clz_valid;
 			end
 			floating[BX - 1] <= uniform_pipe[BX - 1];  // symm
 			floating[BX - 2] <= uniform_pipe[BX - 2];  // part
 			floating[MANT_BW-1:0] <= uniform_pipe[MANT_BW-1:0];  // mantissa
-			data_valid <= clz_valid;  // TODO: implement system to consume more random numbers if necessary (de Schryver et al)
+
 		end
 	end
+
+	clz_clk #(.bits_in(EXP_BW)) count_leading_zeros (
+		.b(uniform[MANT_BW +: EXP_BW]),
+		.clk(clk),
+		.rst(rst),
+		.vout(clz_valid),
+		.pout(clz_out[0 +: `CLOG2(EXP_BW)])
+	);
+
 endmodule  // rng_uniform_to_float
 
 module rng_lookup(
-	input clk, en
+	input clk, en,
  	input [EXP_BW:0] section_addr,
 	input[K-1:0] subsection_addr,
 	output reg [BY - 1:0] c0, c1
@@ -145,9 +115,8 @@ module rng_lookup(
 endmodule  // rng_lookup
 
 module rng(
-	input comparator_output,
-	input clk, rst
-	output reg [BY - 1:0] out
+	input clk, rst, bits_in,
+	output reg [BY - 1:0] rng
 	);
 
 	parameter BX = `URNG_BX;
@@ -155,57 +124,67 @@ module rng(
 	parameter BY = `RNG_BY;
 	parameter EXP_BW = `RNG_EXP_BW;
 	parameter K = `RNG_K;
-	parameter OFFSET = RNG_GROWING_OCT * (1'b1 << K);// >=RNG_GROWING_OCT * 2^RNG_K
+	parameter OFFSET = `RNG_GROWING_OCT * (1'b1 << K);// >=RNG_GROWING_OCT * 2^RNG_K
 
-	wire [1:0] urng_rst;  // urng can be reset by uniform_to_float or by module reset
-	wire urng_rst_wire;
-	assign urng_rst_wire = urng_rst[0] | urng_rst[1];
-
+	wire urng_rst, urng_refresh;
 	wire [BX - 1:0] urng_out;
-	wire urng_valid;
+	wire [BX - 1:0] urng_valid;
 
 	wire [BX-1:0] float_out;
 	wire float_valid;
 
-	rng_nonzero_exp_urng #(.N(BX)) urng(
-		.comparator_output(comparator_output),
-		.clk(clk),
-		.rst(urng_rst_wire),
-		.out(urng_out),
-		.valid(urng_valid)
-		);
+	wire float_part;
+	wire [EXP_BW - 1:0] float_exponent;
+	wire [EXP_BW:0] lookup_section_addr;
+	wire [K-1:0] lookup_subsection_addr;
+	wire [BY - 1:0] lookup_c0, lookup_c1;
+
+	assign urng_rst = rst | urng_refresh;
+
+	assign float_part = float_out[MANT_BW + EXP_BW];
+	assign float_exponent = float_out[MANT_BW+EXP_BW - 1:MANT_BW];
+	assign lookup_section_addr = ( float_part == 0 )? {1'b0,float_exponent} : {1'b0,float_exponent} + OFFSET;  // >=RNG_GROWING_OCT * 2^RNG_K
+	assign lookup_subsection_addr = float_out[MANT_BW - 1:MANT_BW - K];
+
+	uniform_rng #(.N(BX)) urng(
+	  .comparator_output(bits_in),
+	  .clk(clk),
+	  .rst(urng_rst),
+	  .out(urng_out),
+	  .valid(urng_valid)
+	);
 
 	rng_uniform_to_float #(.BX(BX), .MANT_BW(MANT_BW)) u_to_f(
 		.clk(clk),
 		.rst(rst),
+		.urng_valid(urng_valid[BX-1]),
 		.uniform(urng_out),
 		.floating(float_out),
-		.data_valid(float_valid),
-		.rst_urng(urng_rst[0])
+		.float_valid(float_valid),
+		.rst_urng(urng_refresh)
+	);
+
+	rng_lookup lookup(
+		.clk(clk),
+		.en(float_valid),
+		.section_addr(lookup_section_addr),
+		.subsection_addr(lookup_subsection_addr),
+		.c0(lookup_c0),
+		.c1(lookup_c1)
 	);
 
 	always @ ( posedge clk ) begin
-		if (rst==0) begin
-
-		end
-	end
-
-	always @ ( posedge clk ) begin
-		if (rst==0) begin
-
-		end
-	end
-
-	always @ ( posedge clk ) begin
 		if (rst) begin
-			out <= 0;
-			urng_rst[1] <= 1;
+			rng <= 0;
+		end
+		else begin
+			rng <= lookup_c0 + lookup_c1 * float_out[MANT_BW-K-1:0];  // TODO: use hardware multiplier (SB_MAC16) if possible
 		end
 	end
-endmodule;  // rng
+endmodule  // rng
 
 // module rng(
-// 	input comparator_output,
+// 	input bits_in,
 // 	input clk, rst
 // 	input read,
 // 	output reg [BY - 1:0] out
@@ -240,7 +219,7 @@ endmodule;  // rng
 // 	reg [BY - 1:0] lookup_c0, lookup_c1;
 //
 // 	rng_nonzero_exp_urng #(.BX(BX)) urng(
-// 		.comparator_output(comparator_output),
+// 		.comparator_output(bits_in),
 // 		.clk(clk),
 // 		.rst(rst),
 // 		.out(urng_out),
@@ -252,7 +231,7 @@ endmodule;  // rng
 // 		.rst(rst),
 // 		.uniform(urng_out),
 // 		.floating(float_out),
-// 		.data_valid(float_valid)
+// 		.float_valid(float_valid)
 // 	);
 //
 // 	rng_lookup lookup(
